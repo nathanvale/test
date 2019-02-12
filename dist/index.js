6b683975ee39bf4473cbd7bbbb89d6be229663ab
Nathan Vale <hireme@nathanvale.com>
2019-02-13 07:44:28 +1100

    inital commit

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.y = exports.x = exports.C = void 0;
+
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+class C {
+  constructor() {
+    this.x = 10;
+
+    this.getX = () => this.x;
+
+    this.setX = newVal => {
+      this.x = newVal;
+    };
+  }
+
+}
+
+exports.C = C;
+let x = new C();
+exports.x = x;
+let y = (0, _extends2.default)({}, {
+  some: "value"
+});
+exports.y = y;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJuYW1lcyI6WyJDIiwieCIsImdldFgiLCJzZXRYIiwibmV3VmFsIiwieSIsInNvbWUiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQU8sTUFBTUEsQ0FBTixDQUFRO0FBQUE7QUFBQSxTQUNMQyxDQURLLEdBQ0QsRUFEQzs7QUFBQSxTQUViQyxJQUZhLEdBRU4sTUFBTSxLQUFLRCxDQUZMOztBQUFBLFNBR2JFLElBSGEsR0FHTEMsTUFBRCxJQUFvQjtBQUFFLFdBQUtILENBQUwsR0FBU0csTUFBVDtBQUFrQixLQUhsQztBQUFBOztBQUFBOzs7QUFNUixJQUFJSCxDQUFDLEdBQUcsSUFBSUQsQ0FBSixFQUFSOztBQUNBLElBQUlLLENBQUMsOEJBQVE7QUFBRUMsRUFBQUEsSUFBSSxFQUFFO0FBQVIsQ0FBUixDQUFMIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIEMge1xuICBwcml2YXRlIHggPSAxMFxuICBnZXRYID0gKCkgPT4gdGhpcy54O1xuICBzZXRYID0gKG5ld1ZhbDogbnVtYmVyKSA9PiB7IHRoaXMueCA9IG5ld1ZhbDsgfVxufVxuXG5leHBvcnQgbGV0IHggPSBuZXcgQygpO1xuZXhwb3J0IGxldCB5ID0geyAuLi57IHNvbWU6IFwidmFsdWVcIiB9IH0iXX0=
